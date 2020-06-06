e37a6ee6a2b1d91d004aba8a57eaf163f151cb6f
jchan3990 <jonchan3990@gmail.com>
2020-06-06 15:33:55 -0700

    Pseudoed the middleware auth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,7 +2,31 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+  if (!req.cookies.shortlyid) {
+    models.Sessions.create()
+      .then((hashData) => {
+        return models.Sessions.get({id: hashData.insertId});
+      })
+      .then((sessionData) => {
+        return res.cookie('shortlyid', sessionData.hash);
+      })
+      .then(session => {
+        // console.log(session.cookie, 'asfsf')
+        req.session = session;
+      });
+  } else {
+    var hash = req.cookies.shortlyid;
+    models.Sessions.get({hash});
 
+  }
+
+  // Check for cookies
+  // If cookies exist
+  // Session exists (returning user)
+  // Get and set session
+  // If cookies don't exist
+  // Create new session
+  // Get and set session
 };
 
 /************************************************************/
